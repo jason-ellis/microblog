acc62f1f6e2eb128df3cff4aee8ab16003e2cabd
Jason Ellis <je7979@gmail.com>
2015-01-18 14:37:16 -0600

    add db+scripts, post class

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 # Load WTForms config from config.py
 app.config.from_object('config')
+db = SQLAlchemy(app)
 
 # import after after app variable to prevent circular import
-from app import views
-
+from app import views, models
